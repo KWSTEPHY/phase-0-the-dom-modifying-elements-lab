78fc877c37d943be44d7da92dbb3dac79f49408c
stephy <stephshiro35@gmail.com>
2024-03-16 18:45:15 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.parentNode.removeChild(mainElement);
+
+var newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+
+newHeader.textContent = "Steph is the champion";
